7a13f9cdd284863f93e6498d22ff3893fafe97b0
matichewer <schwerdt.matias@gmail.com>
2022-05-01 11:44:18 -0300

    grilla de botones onClick X

--- src/Square.js
+++ src/Square.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { colorToCss } from './Game';
 
 class Square extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {text: ""};
+    }
     render() {
         return (
-            <div style={{ backgroundColor: colorToCss(this.props.value) }} />
+            <div style={{ backgroundColor: colorToCss(this.props.value) }} >
+                <button class="miBoton" 
+                    onClick={() => {
+                       this.setState({text:"X"})
+                    } 
+                }>{this.state.text}
+                </button>
+            </div>
         );
     }
 }
