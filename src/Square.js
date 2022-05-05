de2d82a23debff5dadd7076f8a90b29313b40d8c
Matías David Schwerdt <schwerdt.matias@gmail.com>
2022-05-05 10:08:08 -0300

    1 boton deshabilitado de Square

--- src/Square.js
+++ src/Square.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import { colorToCss } from './Game';
+//import {useState} from 'react';
 
 class Square extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {text: ""};
         this.state = {
             value: null,
-        };
+           // botonActivo: true
+        };        
     }
     render() {
         return (
             <div style={{ backgroundColor: colorToCss(this.props.value) }} >
-                <button class="miBoton"
+                <button class="miBoton" disabled={!this.state.botonActivo} 
                     onClick={() => {
-                       this.setState({value:"X"})
-                        console.log('color: ' + this.props.value)
+                        if (!this.props.botonActivo) {
+                            //this.setState({botonActivo: false})
+                            this.setState({value:"X"})
+                            console.log('color: ' + this.props.value)
+                        }
                     } 
-                }>{this.state.value}
+                }>
+                    
+                    {this.state.value}
                 </button>
             </div>
         );
+
+
     }
 }
 export default Square;
